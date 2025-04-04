03b6747f81448ba8ca069c03fc67aebab5097905
MonishaDR-25 <monishadr25@gmail.com>
2025-04-04 15:26:34 +0530

    push to git

---
+++ Ticket/src/in/moni/Tickets/Passenger.java
@@ -0,0 +1,14 @@
+package in.moni.Tickets;
+
+public class Passenger {
+    String name;
+    String address;
+
+    public Passenger(String name,String address){
+
+
+    }
+    public void display() {
+        System.out.println("Passenger Name: " + name +  "address:"+address);
+    }
+}
